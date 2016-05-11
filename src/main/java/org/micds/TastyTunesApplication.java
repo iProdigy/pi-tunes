9d36375cd5d94e0a8d75f79caa55b9af98d8563a
iProdigy <prodigycreep@gmail.com>
2016-05-10 21:06:47 -0500

    Get basic server running

--- src/main/java/org/micds/TastyTunesApplication.java
+++ src/main/java/org/micds/TastyTunesApplication.java
@@ -2,11 +2,23 @@ package org.micds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class TastyTunesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TastyTunesApplication.class, args);
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String home() {
+        return "Tasty Tunes";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyTunesApplication.class, args);
+    }
+
 }
